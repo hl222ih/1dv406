c9924719bc9a0b77ed02a393710c725cd3d83e3e
hl222ih <hl222ih@student.lnu.se>
2014-01-29 10:05:15 +0100

    +klass som utför själva beräkningen av antalet versaler

---
+++ labb1.1/labb1.1/Model/TextAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace labb1._1.Model
+{
+    public static class TextAnalyzer
+    {
+        /// <summary>
+        /// Beräknar antalet versaler i inskickad sträng.
+        /// </summary>
+        /// <param name="text">Sträng som ska testas.</param>
+        /// <returns>Antalet versaler i strängen.</returns>
+        public static int GetNumberOfCapitals(string text)
+        {
+            return text.Count(c => char.IsLetter(c) && c.Equals(char.ToUpper(c)));
+        }
+    }
+}
