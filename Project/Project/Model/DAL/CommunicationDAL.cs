b61f4d393c1ab0d308c05516b7f17f6a026aeebe
Hannes <hl222ih@student.lnu.se>
2014-03-10 22:28:03 +0100

    +metoder SelectAllMeanings och SelectAllItems

--- Project/Project/Model/DAL/CommunicationDAL.cs
+++ Project/Project/Model/DAL/CommunicationDAL.cs
@@ -31,19 +31,105 @@ namespace Project.Model.DAL
 
                         while (reader.Read())
                         {
-                            //var test = reader.GetInt32(wTypeIdIndex);
-                        
-
                             wordTypes.Add(new WordType
                             {
-                                WTypeId = (int)reader.GetByte(wTypeIdIndex),
-                                ColorId = (int)reader.GetByte(colorIdIndex),
+                                WTypeId = reader.GetByte(wTypeIdIndex),
+                                ColorId = reader.GetByte(colorIdIndex),
                                 WType = reader.GetString(wTypeIndex)
                             });
                         }
                     }
 
                     return wordTypes;
+
+                }
+                catch
+                {
+                    throw new ApplicationException("Misslyckades med att hämta information från databasen.");
+                }
+            }
+        }
+
+        public IEnumerable<Meaning> SelectAllMeanings()
+        {
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var meanings = new List<Meaning>();
+
+                    var cmd = new SqlCommand("appSchema.usp_SelectAllMeanings", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var meaningIdIndex = reader.GetOrdinal("MeaningId");
+                        var wTypeIdIndex = reader.GetOrdinal("WTypeId");
+                        var wordIndex = reader.GetOrdinal("Word");
+                        var commentIndex = reader.GetOrdinal("Comment");
+
+                        while (reader.Read())
+                        {
+                            meanings.Add(new Meaning
+                            {
+                                MeaningId = reader.GetInt16(meaningIdIndex),
+                                WTypeId = reader.GetByte(wTypeIdIndex),
+                                Word = reader.GetString(wordIndex),
+                                Comment = reader.GetString(commentIndex)
+                            });
+                        }
+                    }
+
+                    return meanings;
+
+                }
+                catch
+                {
+                    throw new ApplicationException("Misslyckades med att hämta information från databasen.");
+                }
+            }
+        }
+
+        public IEnumerable<Item> SelectAllItems()
+        {
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var items = new List<Item>();
+
+                    var cmd = new SqlCommand("appSchema.usp_SelectAllItems", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+
+                        var itemIdIndex = reader.GetOrdinal("ItemId");
+                        var meaningIdIndex = reader.GetOrdinal("MeaningId");
+                        var imageIdIndex = reader.GetOrdinal("ImageId");
+                        var catRefIdIndex = reader.GetOrdinal("CatRefId");
+                        var catIdIndex = reader.GetOrdinal("CatId");
+                        var posIdIndex = reader.GetOrdinal("PosId");
+
+                        while (reader.Read())
+                        {
+                            items.Add(new Item
+                            {
+                                ItemId = reader.GetInt16(itemIdIndex),
+                                MeaningId = reader.GetInt16(meaningIdIndex),
+                                ImageId = reader.GetInt16(imageIdIndex),
+                                CatRefId = reader.GetInt16(catRefIdIndex),
+                                CatId = reader.GetInt16(catIdIndex),
+                                PosId = reader.GetByte(posIdIndex)
+                            });
+                        }
+                    }
+
+                    return items;
 
                 }
                 catch
